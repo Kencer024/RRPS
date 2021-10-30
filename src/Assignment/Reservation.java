ea671ba08f9036e28766ede11abedf7ced52928d
Kencer024 <KLOW024@e.ntu.edu.sg>
2021-10-30 16:55:28 +0800

    amended reserve to input pax instead of tableId so it will check empty table then like that lor

--- src/Assignment/Reservation.java
+++ src/Assignment/Reservation.java
@@ -1,67 +1,88 @@
 package Assignment;
 
 import java.util.Arrays;
+import java.util.Scanner;
+import java.time.LocalDate; // import the LocalDate class
 
 public class Reservation {
-    // private Table[] table = new Table[30];      //assume 30 tables
+    private Table[] table = new Table[30];      //assume 30 tables
+    Scanner sc = new Scanner(System.in);
 
+    public Reservation(){
+            for(int i =0; i< table.length;i++){
+                table[i] = new Table(i);
+                table[i].setTableId(i);
+                table[i].setReserved(false);
+                if(i < 10){                     //1st 10 tables 2 pax
+                    table[i].setPax(4);
 
-    // public Reservation(){
-    //         for(int i =0; i< table.length;i++){
-    //             table[i] = new Table(i);
-    //         }
-    // }
+                }
+                else if(i<20){                  //2nd 10 tables 4 pax
+                    table[i].setPax(6);
+                }
+                else
+                    table[i].setPax(8);
+            }
+    }
 
-    // public void reserve(int tableId){
-    //     for(int i =0; i< table.length;i++){             //brute force search
-    //         if(table[i].getTableId() == tableId){       //if table[i] is tableid
-    //             table[i].setReserved(true);             //set it to reserved
-    //             System.out.println("Table is reserved");
-    //         }
-    //     }
-    // }
+    public void reserve(int pax){
+        checkEmptyTable(pax);
+        boolean doesTableExist = false;
+        System.out.println("Enter choice of Table : ");
+        int tableId = sc.nextInt();
+        for(int i =0; i< table.length;i++){             //brute force search
+            if(table[i].getTableId() == tableId && table[i].getreserved() != false) {       //if table[i] is tableid
+                table[i].setReserved(true);             //set it to reserved
+                System.out.println("Table is reserved");
+                doesTableExist = true;
+            }
+        }if(doesTableExist == false){
+            System.out.println("Table does not exist");
+        }
 
-    // public void removeReservation(int tableId){
-    //     for(int i =0; i< table.length;i++){             //brute force search
-    //         if(table[i].getTableId() == tableId){       //if table[i] is tableid
-    //             table[i].setReserved(false);             //set it to !reserved
-    //         }
-    //     }
-    // }
+    }
 
-    // public boolean checkReservation(int tableId){       //checks whether a certain table is reserved
-    //     for(int i =0; i< table.length;i++){             //brute force search
-    //         if(table[i].getTableId() == tableId){       //if table[i] is tableid
-    //             return table[i].getreserved();             //set it to !reserved
-    //         }
-    //         else{
-    //             System.out.println("Table doesnt exist");
-    //         }
-    //     }return false;
-    // }
+    public void removeReservation(int tableId){
+        for(int i =0; i< table.length;i++){             //brute force search
+            if(table[i].getTableId() == tableId){       //if table[i] is tableid
+                table[i].setReserved(false);             //set it to !reserved
+            }
+        }
+    }
 
-    // public void updateReservation(int tableId, int tableId2){
-    //     for(int i =0; i< table.length;i++){             //brute force search
-    //         if(table[i].getTableId() == tableId) {       //if table[i] is tableid
-    //                 table[i].setReserved(false);
-    //         }
-    //         if(table[i].getTableId() == tableId2){
-    //             table[i].setReserved(true);
-    //         }
-    //     }
-    // }
+    public boolean checkReservation(int tableId){       //checks whether a certain table is reserved
+        for(int i =0; i< table.length;i++){             //brute force search
+            if(table[i].getTableId() == tableId){       //if table[i] is tableid
+                return table[i].getreserved();             //set it to !reserved
+            }
+            else{
+                System.out.println("Table doesnt exist");
+            }
+        }return false;
+    }
 
-    // public Table[] checkEmptyTable(){
-    //     Table[] eTable = new Table[0];
-    //     for(int i =0;i< table.length; i++){
-    //         if(table[i].getreserved() == false){
-    //             System.out.println(table[i].getTableId() + " is empty ");
-    //             eTable = Arrays.copyOf(eTable, eTable.length+1);        //append eTable array
-    //             eTable[eTable.length-1] = table[i];
-    //         }
-    //     }
-    //     return eTable;
-    // }
+    public void updateReservation(int tableId, int tableId2){
+        for(int i =0; i< table.length;i++){             //brute force search
+            if(table[i].getTableId() == tableId) {       //if table[i] is tableid
+                    table[i].setReserved(false);
+            }
+            if(table[i].getTableId() == tableId2){
+                table[i].setReserved(true);
+            }
+        }
+    }
+
+    public Table[] checkEmptyTable(int pax){
+        Table[] eTable = new Table[0];
+        for(int i =0;i< table.length; i++){
+            if(table[i].getreserved() == false && table[i].getpax() >= pax){
+                System.out.println(table[i].getTableId() + " is empty ");
+                eTable = Arrays.copyOf(eTable, eTable.length+1);        //append eTable array
+                eTable[eTable.length-1] = table[i];
+            }
+        }
+        return eTable;
+    }
 
 
 }
