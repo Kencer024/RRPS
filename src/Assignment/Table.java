2febe3bd10b908bef975480656fbb617dd6fd711
Kencer024 <KLOW024@e.ntu.edu.sg>
2021-10-30 13:46:03 +0800

    error

--- src/Assignment/Table.java
+++ src/Assignment/Table.java
@@ -3,11 +3,11 @@ package Assignment;
 abstract class Table {
     private int tableId;
     private boolean reserved;
-    private int pax;
+
 
     public int getTableId(){return this.tableId;}
     public boolean getreserved(){return this.reserved;}
-    public int getpax(){return this.pax;}
+
 
     public void setTableId(int tableId){this.tableId = tableId;}
 
