3f16f130090e507f73cf8b35091e6612634d2506
Kencer024 <KLOW024@e.ntu.edu.sg>
2021-10-30 13:46:03 +0800

    fixed issue

--- src/Assignment/Main.java
+++ src/Assignment/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Reservation[] r = new Reservation[30];
+        r[0].checkEmptyTable();
     }
 }
