2353aa00a6701b6dd6ce5c07ebec74712ed70ac7
Patipol-BKK <patipol.ti@gmail.com>
2021-11-05 15:28:39 +0800

    Update SalesDatabase.java

--- src/Assignment/SalesDatabase.java
+++ src/Assignment/SalesDatabase.java
@@ -3,4 +3,10 @@ import java.util.ArrayList;
 public class SalesDatabase
 {
 	// ArrayList
+	private ArrayList<Order> previousOrders;
+
+	public OrderDatabase()
+	{
+		previousOrders = new ArrayList<Order>(0);
+	}
 }
