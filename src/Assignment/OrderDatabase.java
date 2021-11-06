d939694b6d2b7879cd42a5682f553cb4f4748ce6
Ken <KLOW024@e.ntu.edu.sg>
2021-11-06 23:00:28 +0800

    javadocs for OrderDatabase

--- src/Assignment/OrderDatabase.java
+++ src/Assignment/OrderDatabase.java
@@ -2,10 +2,17 @@ package Assignment;
 
 import java.util.ArrayList;
 
+/** Represents a class to store an array of orders to be called by the main function
+ * to act as a data storage
+ *
+ */
 public class OrderDatabase
 {
 	private ArrayList<Order> currentOrders;
 
+	/** Initialising an array of Order
+	 *
+	 */
 	public OrderDatabase()
 	{
 		currentOrders = new ArrayList<Order>();
@@ -35,6 +42,11 @@ public class OrderDatabase
 		return left;
 	}
 
+	/** Used to search for the order object by tableId
+	 *
+	 * @param tableId A string representing the identity of the table
+	 * @return Order object with the tableId input
+	 */
 	public Order searchOrder(String tableId)
 	{
 		int index;
@@ -48,11 +60,18 @@ public class OrderDatabase
 		return null;
 	}
 
+	/** Getter method to retrieve an array of order objects
+	 *
+	 * @return An array of order object
+	 */
 	public ArrayList<Order> getCurrentOrders()
 	{
 		return currentOrders;
 	}
 
+	/**
+	 * Prints all the customer orders at that instance
+	 */
 	public void printCurrentOrders()
 	{
 		int index;
@@ -61,6 +80,13 @@ public class OrderDatabase
 			System.out.println("Table " + currentOrders.get(index).getTableId());
 		}
 	}
+
+	/** Creates a new order and searches for duplicates
+	 *
+	 * @param tableId A string representing the identity of the table
+	 * @param pax An integer representing the current number of people
+	 *            allocated to the table
+	 */
 	public void newOrder(String tableId, int pax)
 	{
 		Order newOrder = new Order(tableId, pax);
@@ -97,6 +123,11 @@ public class OrderDatabase
 		currentOrders.add(left, newOrder);
 	}
 
+	/** Removes an order from the array of order
+	 *
+	 * @param tableId A string represent the identity of the table
+	 * @return A new array of order that removed the order with the tableId inputted
+	 */
 	public Order removeOrder(String tableId)
 	{
 		int orderIndex = getOrderIndex(tableId);
@@ -107,6 +138,14 @@ public class OrderDatabase
 		return tmp;
 	}
 
+	/** Adds items to the order, i.e add to cart
+	 *
+	 * @param tableId A string representing the table identity
+	 * @param itemId A string representing the type of item to add
+	 * @param amount An integer representing the cost of the item
+	 * @return Boolean that returns true if the item is added successfully
+	 * and false if order is not found
+	 */
 	public Boolean addItem(String tableId, String itemId, int amount)
 	{
 		int orderIndex = getOrderIndex(tableId);
@@ -115,6 +154,13 @@ public class OrderDatabase
 		return true;
 	}
 
+	/** Removes item from the order in the array of order
+	 *
+	 * @param tableId A string representing the table identity
+	 * @param itemId A string representing the type of item to add
+	 * @return Boolean that returns true if item is removed successfully and false
+	 * if order is not found
+	 */
 	public Boolean removeItem(String tableId, String itemId)
 	{
 		if(currentOrders.size() == 0)return false;
@@ -124,6 +170,14 @@ public class OrderDatabase
 		return true;
 	}
 
+	/** Adds promotion set to the order object which is saved in an array of order
+	 *
+	 * @param tableId A string representing the table identity
+	 * @param setId A string representing the type of promotion set to add
+	 * @param amount An integer representing the cost of the promotion set
+	 * @return Boolean that returns true if the order with the tableId inputted is found
+	 * hence addition is successful, else false when no such order is found
+	 */
 	public Boolean addSet(String tableId, String setId, int amount)
 	{
 		int orderIndex = getOrderIndex(tableId);
@@ -132,6 +186,13 @@ public class OrderDatabase
 		return true;
 	}
 
+	/** Removes promotion set from the order object which is saved in an array of order
+	 *
+	 * @param tableId A string representing the table identity
+	 * @param setId A string representing the type of promotion set to add
+	 * @return Boolean that returns true if the order with the tableId inputted is found
+	 * hence removal is successful, else false when no such order is found
+	 */
 	public Boolean removeSet(String tableId, String setId)
 	{
 		int orderIndex = getOrderIndex(tableId);
@@ -140,18 +201,36 @@ public class OrderDatabase
 		return true;
 	}
 
+	/** Getter method to retrieve array of items of tableId with its respective items
+	 *
+	 * @param tableId A string representing the identity of table
+	 * @return Array of dictionary of the tableId and its respective item
+	 */
 	public ArrayList<Pair<String, Integer>> listItems(String tableId)
 	{
 		int orderIndex = getOrderIndex(tableId);
 		if(!currentOrders.get(orderIndex).getTableId().matches(tableId)) return null;
 		return currentOrders.get(getOrderIndex(tableId)).listItems();
 	}
+
+	/** Getter method to retrieve array of items of tableId with its respective sets
+	 *
+	 * @param tableId A string representing the identity of table
+	 * @return Array of dictionary of the tableId and its respective sets
+	 */
 	public ArrayList<Pair<String, Integer>> listSets(String tableId)
 	{
 		int orderIndex = getOrderIndex(tableId);
 		if(!currentOrders.get(orderIndex).getTableId().matches(tableId)) return null;
 		return currentOrders.get(getOrderIndex(tableId)).listSets();
 	}
+
+	/** Clears the order of tableId
+	 *
+	 * @param tableId A string representing the identity of table
+	 * @return Boolean that returns true if the order with the tableId inputted is found
+	 * hence clearing is successful, else false when no such order is found
+	 */
 	public Boolean clearAll(String tableId)
 	{
 		int orderIndex = getOrderIndex(tableId);
@@ -160,6 +239,12 @@ public class OrderDatabase
 		return true;
 	}
 
+	/** Prints the item with the tableId inputted
+	 *
+	 * @param tableId A string representing the identity of table
+	 * @return Boolean that returns true if the order with the tableId inputted is found
+	 * hence printing is successful, else false when no such order is found
+	 */
 	public Boolean printItems(String tableId)
 	{
 		int orderIndex = getOrderIndex(tableId);
@@ -168,6 +253,12 @@ public class OrderDatabase
 		return true;
 	}
 
+	/** Prints the set with the tableId inputted
+	 *
+	 * @param tableId A string representing the identity of table
+	 * @return Boolean that returns true if the order with the tableId inputted is found
+	 * hence printing is successful, else false when no such order is found
+	 */
 	public Boolean printSets(String tableId)
 	{
 		int orderIndex = getOrderIndex(tableId);
@@ -176,6 +267,12 @@ public class OrderDatabase
 		return true;
 	}
 
+	/** Applies membership for the order with tableId
+	 *
+	 * @param tableId A string representing the identity of table
+	 * @return Boolean that returns true if the order with the tableId inputted is found
+	 * hence addition is successful, else false when no such order is found
+	 */
 	public Boolean applyMembership(String tableId)
 	{
 		int orderIndex = getOrderIndex(tableId);
@@ -184,6 +281,12 @@ public class OrderDatabase
 		return true;
 	}
 
+	/** Checks if a table is in the order list
+	 *
+	 * @param tableId A string representing the identity of table
+	 * @return Boolean that returns true if the order with the tableId inputted is found,
+	 * else false when no such order is found
+	 */
 	public Boolean isInOrderList(String tableId)
 	{
 		int orderIndex = getOrderIndex(tableId);
@@ -191,6 +294,13 @@ public class OrderDatabase
 		return true;
 	}
 
+	/** Computes the bill of a specific order
+	 *
+	 * @param tableId A string representing the identity of table
+	 * @param menu A class for holding all the full objects of items
+	 *             and promotional sets in the menu
+	 * @return A float representing the total bill calculated
+	 */
 	public float computeBill(String tableId, MenuList menu)
 	{
 		int orderIndex = getOrderIndex(tableId);
@@ -198,6 +308,13 @@ public class OrderDatabase
 		return currentOrders.get(getOrderIndex(tableId)).computeBill(menu);
 	}
 
+	/** Prints the bill of a specific order
+	 *
+	 * @param tableId A string representing the identity of table
+	 * @param menu A class for holding all the full objects of items
+	 *             and promotional sets in the menu
+	 * @return Boolean that is true if the order is found and false otherwise
+	 */
 	public Boolean printBill(String tableId, MenuList menu)
 	{
 		int orderIndex = getOrderIndex(tableId);
@@ -206,6 +323,10 @@ public class OrderDatabase
 		return true;
 	}
 
+	/** Prints the date and time of a specific order when they first ordered
+	 *
+	 * @param tableId A string representing the identity of table
+	 */
 	public void printDateTime(String tableId)
 	{
 		int orderIndex = getOrderIndex(tableId);
@@ -213,6 +334,10 @@ public class OrderDatabase
 		currentOrders.get(getOrderIndex(tableId)).printDateTime();
 	}
 
+	/** Returns the size of the current order
+	 *
+	 * @return An integer representing the size of current order
+	 */
 	public int size()
 	{
 		return currentOrders.size();
