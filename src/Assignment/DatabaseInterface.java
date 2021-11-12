e98b6d61a0c990f049e127925fa3646b9b0ff334
vamsig24 <vamsi24g@gmail.com>
2021-11-12 15:34:29 +0800

    Updated SalesDatabase

--- src/Assignment/DatabaseInterface.java
+++ src/Assignment/DatabaseInterface.java
@@ -2,10 +2,10 @@ package Assignment;
 
 public interface DatabaseInterface {
 
-    public Order getOrder();
+    public Order getOrderInfo(String orderId_input);
     public void printDatabase();
-    public void appendOrder();
-    public void removeOrder();
+    public void appendOrder(Order order_add);
+    public void removeOrder(String order_id);
     public int getTotalNumelements();
     
 }
